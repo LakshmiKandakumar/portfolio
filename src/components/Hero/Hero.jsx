b77aa41d474d6fef3e64678e143849f00e89baac
LakshmiKandakumar <121855953+LakshmiKandakumar@users.noreply.github.com>
2025-06-24 02:06:22 +0530

    Updated responsiveness in Hero and Project components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -7,20 +6,21 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, Lakshmi K</h1>
+        <h1 className={styles.title}>Hi, I'm Lakshmi K</h1>
         <p className={styles.description}>
           Final-year B.Tech AIML student at RMD Engineering College | Skilled in AI, ML, and full-stack development | Seeking entry-level tech roles
-
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
+
       <img
         src={getImageUrl("hero/heroImage.png")}
-        alt="Hero image of me"
+        alt="Lakshmi's profile"
         className={styles.heroImg}
       />
+
       <div className={styles.topBlur} />
       <div className={styles.bottomBlur} />
     </section>
